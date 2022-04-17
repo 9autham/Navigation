74bc4d71cfb7fd4bb0e24e87f6b767199c76f485
KURUVA GAUTHAM KUMAR <91959504+9autham@users.noreply.github.com>
2022-04-17 15:52:48 +0530

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,39 @@
+import React from "react";
+import imageOne from "../components/images/img1.jpg";
+function Home() {
+  return (
+    <div className="text-center">
+      <p className="display-2 text-info">Welcome to my app</p>
+      <img scr={imageOne} className="w-50" alt="img" />
+      <p className="lead">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and
+        typesetting industry. Lorem Ipsum has been the industry's standard dummy
+        text ever since the 1500s, when an unknown printer took a galley of type
+        and scrambled it to make a type specimen book. It has survived not only
+        five centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and
+        typesetting industry. Lorem Ipsum has been the industry's standard dummy
+        text ever since the 1500s, when an unknown printer took a galley of type
+        and scrambled it to make a type specimen book. It has survived not only
+        five centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Users.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+function Users() {
+  let [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://reqres.in/api/users?page=2")
+      .then((res) => res.json())
+      .then((apidata) => setUsers(apidata.data))
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div className="text-center">
+      <p className="display-2 text-secondary">User Data</p>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Email</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Avatar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((userObj) => (
+            <tr key={userObj.id}>
+              <td>{userObj.id}</td>
+              <td>{userObj.email}</td>
+              <td>{userObj.first_name}</td>
+              <td>{userObj.last_name}</td>
+              {/* <td>{userObj.avatar}</td> */}
+              <td>
+                <img scr={userObj.avatar} alt="XS3" />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Users;
